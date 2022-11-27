8115b56a236f948a8c56ce4734bf7a4a90cb3acc
Naqij <naqi.jaffari@gmail.com>
2022-11-27 18:33:30 +0000

    deployment commit

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"]="postgres://vgaxtaxo:YucM_hogdPkY6r5scwjAI_I_MjtOy4UW@lucky.db.elephantsql.com/vgaxtaxo"
 os.environ["SECRET_KEY"]="my_secret_key"
+os.environ["CLOUDINARY_URL"]="cloudinary://118622994499979:TDIqDR3PU64DOQkfZPtZXUYVI_E@dnzocuzpo"
