12691decb6871ca18e9b897c3662b6f4ab8fcbba
Naqij <naqi.jaffari@gmail.com>
2022-11-27 19:06:58 +0000

    attempt to fix bug

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-blog-naqi.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['django-blog-naqi.herokuapp.com', 'localhost']
 
 
 # Application definition
